b0cd32a3518f22f2095685959c8b77bdb7b1e184
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-17 10:15:29 +0530

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by shehan karunarathna on 2022-02-17.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
